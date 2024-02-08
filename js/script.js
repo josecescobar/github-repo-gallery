aa17c2a7267e9f709a4aba0f1d753ffbc40c384e
Jose Escobar <josecapacho@gmail.com>
2024-02-08 11:57:49 -0500

    Introduced global variables for the Back button and search input, implemented click event for gallery return, and added input event to dynamically filter repos by name.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const overview = document.querySelector(".overview");
 const username = "josecescobar";
 const repoList = document.querySelector(".repo-list");
+const allReposContainer = document.querySelector(".repos");
+const repoData = document.querySelector(".repo-data");
+const viewRepostButton = document.querySelector(".view-repos");
+const filterInput = document.querySelector(".filter-repos");
 
 const gitUserInfo = async function () {
   const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -25,16 +29,17 @@ const displayUserInfo = function (data) {
      </div>
      `;
     overview.append(div);
-    gitRepos();
+    gitRepos(username);
 };
 
-const gitRepos = async function () {
+const gitRepos = async function (username) {
  const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
  const repoData = await fetchRepos.json();
  displayRepos(repoData);
 };
 
 const displayRepos = function (repos) {
+   filterInput.classList.remove("hide");
    for (const repo of repos) {
      const repoItem = document.createElement("li");
      repoItem.classList.add("repo");
@@ -53,9 +58,9 @@ repoList.addEventListener("click", function (e) {
 const getRepoInfo = async function (repoName) {
    const fetchInfo = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
    const repoInfo = await fetchInfo.json();
-   console.log(repoInfo);
+
    // Grab languages
-   const fetchLanguages = await fetch (repoInfo.fetchLanguages_url);
+   const fetchLanguages = await fetch (repoInfo.languages_url);
    const languageData = await fetchLanguages.json();
 
    // Make a list of languages 
@@ -68,6 +73,7 @@ const getRepoInfo = async function (repoName) {
 };
 
 const displayRepoInfo = function (repoInfo, languages) {
+   viewRepostButton.classList.remove("hide");
    repoData.innerHTML = "";
    repoData.classList.remove("hide");
    allReposContainer.classList.add("hide");
@@ -81,3 +87,25 @@ const displayRepoInfo = function (repoInfo, languages) {
    `;
    repoData.append(div);
 };
+
+viewRepostButton.addEventListener("click", function () {
+   allReposContainer.classList.remove("hide");
+   repoData.classList.add("hide");
+   viewRepostButton.classList.add("hide");
+});
+
+//// Dynamic search
+filterInput.addEventListener("input", function(e) {
+   const searchText = e.target.value;
+   const repos = document.querySelectorAll(".repo");
+   const searchLowerText = searchText.toLowerCase();
+
+   for (const repo of repos) {
+      const repoLowerText = repo.innerText.toLowerCase();
+      if (repoLowerText.includes(searchLowerText)) {
+         repo.classList.remove("hide");
+      } else {
+         repo.classList.add("hide");
+      }
+   }
+});
